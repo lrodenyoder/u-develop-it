20299d18ed51909705fddce1cf3327a501929de8
lrodenyoder <rodenlau000@gmail.com>
2022-03-13 20:30:28 -0500

    12.3.7

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,20 @@
+-- if table A(parties) depends on table B(candidates), table B MUST be deleted before A 
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+
+-- if table A(parties) depends on table B(candidates), table A MUST be defined before B 
+CREATE TABLE parties (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+--   ON DELETE SET NULL to tell SQL to set a candidate's party_id field to NULL if the corresponding row in parties is ever deleted
 );
--- server.js
+++ server.js
@@ -24,7 +24,12 @@ const db = mysql.createConnection(
 //get all candidates
 //variables are assigned to make query more legible
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    //joins the parties table and candidates table. this one joins just the party name on the left side to the candidate table
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -41,7 +46,13 @@ app.get('/api/candidates', (req, res) => {
 //GET a single candidate
 // ? denotes a placeholder making it a PREPARED STATEMENT. can execute sql statements repeatedly using different values
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    //WHERE clause has to be at the end of the statement
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -105,7 +116,92 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+//update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
 
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `UPDATE candidates SET party_id = ?
+    WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            //check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found',
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
+//get all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//get party by id
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//delete party by id
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            //checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 
 
